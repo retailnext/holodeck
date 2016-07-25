880ae4f971bdf70dc7e05daa528dfa0df69d77e6
Muir Manders <muir@retailnext.net>
2016-07-25 08:45:41 -0700

    Enhance wesley.
    Now user mention to the offender works properly. No longer need id
    to name mapping as the client displays the username when resolving the
    mention.
    
    Also, format the old and new edited message to improve readability.

--- character/wesley/wesley.go
+++ character/wesley/wesley.go
@@ -12,14 +12,12 @@ func New(c *slack.Client) character.Character {
 	return &wesley{
 		c:              c,
 		recentMessages: make(map[string]*list.List),
-		userNames:      make(map[string]string),
 	}
 }
 
 type wesley struct {
 	c              *slack.Client
 	recentMessages map[string]*list.List
-	userNames      map[string]string
 }
 
 func (w *wesley) Respond(msgEvent *slack.MessageEvent) string {
@@ -41,21 +39,7 @@ func (w *wesley) Respond(msgEvent *slack.MessageEvent) string {
 		for e := l.Front(); e != nil; e = e.Next() {
 			prevMsg := e.Value.(slack.Msg)
 			if prevMsg.Timestamp == msg.Timestamp && prevMsg.Text != msg.Text {
-				name := w.userNames[msg.User]
-				if name == "" {
-					user, err := w.c.GetUserInfo(msg.User)
-					if err != nil {
-						fmt.Printf("error fetching user info for %s: %s", msg.User, err)
-					}
-
-					if user != nil {
-						name = user.Name
-						w.userNames[msg.User] = name
-					} else {
-						name = msg.User
-					}
-				}
-				ret = fmt.Sprintf("@%s edited %q to %q! I'm reporting this to the Captain!", name, prevMsg.Text, msg.Text)
+				ret = fmt.Sprintf("<@%s> edited `%s` to `%s`! I'm reporting this to the Captain!", msg.User, prevMsg.Text, msg.Text)
 				break
 			}
 		}
